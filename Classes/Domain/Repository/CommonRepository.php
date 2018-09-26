400d1bdb2a21fb5d1fdf3827c674f1e4064ff8ec
Oliver Eglseder <php@vxvr.de>
2018-09-26 22:51:38 +0200

    [REFACTOR] Remove the unused database connections from the ReplaceMarkerService constructor

--- Classes/Domain/Repository/CommonRepository.php
+++ Classes/Domain/Repository/CommonRepository.php
@@ -1262,11 +1262,7 @@ class CommonRepository extends BaseRepository
                 $whereClause = '';
                 if (!empty($columnConfiguration['foreign_table_where'])) {
                     /** @var ReplaceMarkersService $replaceMarkers */
-                    $replaceMarkers = GeneralUtility::makeInstance(
-                        ReplaceMarkersService::class,
-                        $this->localDatabase,
-                        $this->foreignDatabase
-                    );
+                    $replaceMarkers = GeneralUtility::makeInstance(ReplaceMarkersService::class);
                     $whereClause = $replaceMarkers->replaceMarkers(
                         $record,
                         $columnConfiguration['foreign_table_where']
